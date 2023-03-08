dd5bb7f44e56e973dc8cbd38cbe84a5b9d81c19d
ahadvirus <ahadvirus@yahoo.com>
2023-03-08 17:40:24 +0330

    Add Blazor MAUI app

---
+++ VpnHood.Client.App.Cross/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace VpnHood.Client.App.Cross;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
